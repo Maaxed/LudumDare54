64d18f0ec6c715387c22e9b1778f2c9be2d644bf
Lucas Guedon <guedon.lucas@laposte.net>
2023-10-02 19:55:02 +0200

    Add baked lights + change lights to red when running out of power

--- Assets/Scripts/ElectricityManager.cs
+++ Assets/Scripts/ElectricityManager.cs
@@ -6,22 +6,35 @@ public class ElectricityManager : MonoBehaviour
 {
     public AudioSource Alarm;
     public Product ProductType;
+    public Material LightMaterial;
+    public Material WhiteLightMaterial;
+    public Material RedLightMaterial;
+    private bool IsAlarmOn = false;
+
+    private void Start()
+    {
+        LightMaterial.CopyMatchingPropertiesFromMaterial(WhiteLightMaterial);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (GameController.Instance.GetProductValue(ProductType) > 0.0f)
         {
-            if (Alarm.isPlaying)
+            if (IsAlarmOn)
             {
                 Alarm.Stop();
+                IsAlarmOn = false;
+                LightMaterial.CopyMatchingPropertiesFromMaterial(WhiteLightMaterial);
             }
         }
         else
         {
-            if (!Alarm.isPlaying)
+            if (!IsAlarmOn)
             {
                 Alarm.Play();
+                IsAlarmOn = true;
+                LightMaterial.CopyMatchingPropertiesFromMaterial(RedLightMaterial);
             }
         }
     }
